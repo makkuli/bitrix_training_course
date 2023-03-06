b099e05e6a06f0c97277e5304c51c2043fe6b500
Maxim <makkuli@yandex.ru>
2023-03-06 14:12:37 +0500

    Добавлена включаемая область (логотип).

---
+++ include/logo.php
@@ -0,0 +1,3 @@
+<div class="logo">
+ <a href="/"><img alt="logo.png" src="/upload/medialibrary/37f/iompwjtsredx5dl2tg1gs8bi3zq1eh73.png" title="logo.png" class="img-responsive" width="164" height="51"></a>
+</div>
